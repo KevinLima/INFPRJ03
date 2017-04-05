811a7f84a7e4ee9a33e61e7e6cc1a3ed65767d61
Dave Caron <0926157@hr.nl>
2017-04-06 01:38:43 +0200

    Improved map

--- assets/modules/map.py
+++ assets/modules/map.py
@@ -2,5 +2,8 @@ import gmplot
 
 class Map:
     def __init__(self):
-        self.gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
+        # (Latitude, Longitude, Zoom level)
+        self.city = (51.9279723, 4.490398, 11)
+        self.gmap = gmplot.GoogleMapPlotter(self.city[0], self.city[1], self.city[2])
         self.gmap.draw("index.html")
+
