332e93db63c26173c6a7b2899863f8725af4fa1c
Dave Caron <0926157@hr.nl>
2017-04-05 23:38:27 +0200

    Changed window title and resolution

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import tkinter
 
 root = tkinter.Tk()
 
+# Set window title
+root.title("INFPRJ03")
+
+# Set window resolution
+root.geometry("800x600")
+
 # Code to add widgets will go here...
 
 root.mainloop()
