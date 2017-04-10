55da823df9f90bfb854963af43112d2f4ab72560
Kevin Lima <cknl360@gmail.com>
2017-04-10 11:54:34 +0200

    get street data per year

--- data.js
+++ data.js
@@ -1,12 +1,30 @@
 class Data{
 	constructor(){
-		this.getData();
+		//this.getData();
+		this.getData(2012);
 	}
 
-	 getData(){
+	getData(){
 		var request = require("request")
 
 		var url = "http://member.kevinlima.com/change.php"
+
+		request({
+		    url: url,
+		    json: true
+		}, function (error, response, body) {
+
+		    if (!error && response.statusCode === 200) {
+		        //console.log(body) // Print the json response
+		        // TODO: Create logic that uses the data 
+		    }
+		})
+	}
+	getData(year){
+		console.log(year);
+		var request = require("request")
+
+		var url = "http://member.kevinlima.com/topstreet.php?year="+year;
 
 		request({
 		    url: url,
