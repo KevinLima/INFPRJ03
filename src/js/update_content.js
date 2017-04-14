cd0384ed2b4390a0600ed8e3f173958634753e81
Dave Caron <0926157@hr.nl>
2017-04-14 19:08:59 +0200

    Added loading bar mechanics

--- src/js/update_content.js
+++ src/js/update_content.js
@@ -1,21 +1,25 @@
 function update_content(option) {
 	switch(option) {
 		case 'Heatmap':
+			// change_visibility('loadingBar', 'visible')
 			change_src('content', './src/html/heat.html');
 			change_visibility('yearSlider', 'visible');
 			break;
 
 		case 'Point Map':
+			// change_visibility('loadingBar', 'visible');
 			change_src('content', './src/html/mark.html');
 			change_visibility('yearSlider', 'visible');
 			break;
 
 		case 'About':
+			change_visibility('loadingBar', 'hidden');
 			change_src('content', './src/html/about.html');
 			change_visibility('yearSlider', 'hidden');
 			break;
 
 		case 'Questions':
+			change_visibility('loadingBar', 'hidden');
 			change_src('content', './src/html/questions.html');
 			change_visibility('yearSlider', 'hidden');
 			break;
