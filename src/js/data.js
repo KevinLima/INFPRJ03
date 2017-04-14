21af39afbce71937e5470e97b697f31d9ee0f2bb
Osman Altun <osman@MacBook-Pro-van-Osman.local>
2017-04-14 16:10:13 +0200

    Performance boost

--- src/js/data.js
+++ src/js/data.js
@@ -1,40 +1,59 @@
-class Data{
+class Data
+{
 	//TODO: implement topbikes_data
 
 	constructor()
-	//The constructor initializes raw_data variable.
-	//If you wish to use a certain year, the following formula applies: 
-	//										this.topstreet_data[year][index][variablename] (E.g. this.topstreet_data[0][200]['Street'])
-	// 										(year -> 0:sum of all years, 1:2011, 2:2012, 3:2013)
 	{
-		this.topstreet_data = [this.getTopStreet(), this.getTopStreet(2011), this.getTopStreet(2012), this.getTopStreet(2013)]
-		this.location_data = this.getLocationData();
+		//this.topstreet_data = [this.getTopStreet(), this.getTopStreet(2011), this.getTopStreet(2012), this.getTopStreet(2013)];
+		//this.location_data = this.getLocationData();
+		this.topstreet_data = [];
+		this.initTopStreetData();
+		this.initLocationData();
 	}
 
-	getLocationData()
+	initLocationData()
 	// creates a list of coordinates and returns the list
 	{
-		var result = [];
-		for(var j = 0; j < this.topstreet_data[0].length; j++)
-		{
-			for(var k = 0; k < this.topstreet_data[0][j]["Count"]; k++)
+		var interval = setInterval(()=>{
+			if(this.topstreet_data[0] !== undefined)
 			{
-				result.push({lat:parseFloat(this.topstreet_data[0][j]['Latitude']), lng:parseFloat(this.topstreet_data[0][j]['Longitude'])})
+				var result = [];
+				for(var j = 0; j < this.topstreet_data[0].length; j++)
+				{
+					for(var k = 0; k < this.topstreet_data[0][j]["Count"]; k++)
+					{
+						result.push({lat:parseFloat(this.topstreet_data[0][j]['Latitude']), lng:parseFloat(this.topstreet_data[0][j]['Longitude'])});
+					}
+				}
+				this.location_data = result;
+				clearInterval(interval);
 			}
-		}
-		return result;
+		}, 100);
 	}
 
-	getTopStreet(year)
+	initTopStreetData(year)
 	{
 		var url = "http://member.kevinlima.com/topstreet.php";
-		if(year != undefined)
+		if(year !== undefined)
 		{
 			url = "http://member.kevinlima.com/topstreet.php?year="+year;
 		}
-		var req = new XMLHttpRequest();
-    	req.open("GET", url, false);
-    	req.send(null);
-    	return JSON.parse(req.responseText);
+		if(year === undefined)
+		{
+			fetch(url, {method:"GET"}).then((response)=>{
+				return response.json();
+			}).then((json)=>{
+				this.topstreet_data.push(json);
+			}).then(()=>{this.initTopStreetData(2011)})
+		}
+		else if (year <= 2013 && year >= 2011)
+		{
+			fetch(url, {method:"GET"}).then((response)=>{
+				return response.json();
+			}).then((json)=>{
+				this.topstreet_data.push(json);
+			}).then(()=>{this.initTopStreetData(year+1);});
+		}
 	}
 }
+dc = new Data(); //dc = dataconnection
